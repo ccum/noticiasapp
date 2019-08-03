79a699b45d9f4fcccdcacb50a8d3fed056b4f82f
Cesar Cueva <cesar.cueva@bbva.com>
2019-08-02 21:24:55 -0500

    Consultando la API de noticias

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, {Component} from 'react';
 
 
 class App extends Component {
-  state = {  }
+  state = {  
+    noticias: []
+  }
+   componentDidMount(){
+    this.consultarNoticias();
+
+  }
+
+  consultarNoticias = async()=>{
+    const url = `https://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=14f9abb922fe4aa8be7be8e75467b9e4`
+    
+    const respuesta= await fetch(url);
+    const noticias = await respuesta.json()
+    this.setState({
+      noticias: noticias.articles
+    })
+  }
   render() { 
     return (<h1>Noticias Api React</h1>);
   }
