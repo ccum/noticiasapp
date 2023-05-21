db7760c41844e96ff6831586f45d70141cb3d523
Cesar <cesar.cueva@bbva.com>
2023-05-21 15:46:52 -0500

    tageo con es_core_news_sm

---
+++ build/precache-manifest.ac698ea6152e9810620a63a02560bb67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebc4d2a84288497c6eb790c5ee548b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bb671114a75e1fc2081",
+    "url": "/static/css/main.db502ba1.chunk.css"
+  },
+  {
+    "revision": "a207ee678f2fd450e51d",
+    "url": "/static/js/2.2020ee69.chunk.js"
+  },
+  {
+    "revision": "7bb671114a75e1fc2081",
+    "url": "/static/js/main.e9bcfd92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
